eed6bfc9ccbcaf297d4ab95238e48ef6abf78122
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-09-29 14:49:32 -0400

    saving to deploy

--- server.js
+++ server.js
@@ -13,9 +13,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
 });
 
 // routes
