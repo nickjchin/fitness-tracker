188f534e2e4fc0a2e107e20b8bae15008d602769
Nicholas Chin <njc94@scarletmail.rutgers.edu>
2021-09-29 00:43:50 -0400

    added type for attribute reps

--- models/workout.js
+++ models/workout.js
@@ -34,6 +34,7 @@ const workoutSchema = new Schema({
       },
       reps: {
         // TODO: For the schema of this attribute, define type as number
+        type: Number,
       },
       sets: {
         // TODO: For the schema of this attribute, define type as number
